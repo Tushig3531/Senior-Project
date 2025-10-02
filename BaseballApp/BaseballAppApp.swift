666169f3a048eaa0193b72bbbad098b57d1435a3
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-10-01 23:11:41 -0500

    Initial Commit

---
+++ BaseballApp/BaseballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseballAppApp.swift
+//  BaseballApp
+//
+//  Created by Tushig Erdenebulgan on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BaseballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
